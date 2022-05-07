433f948757988fc2f42efea4e6e9eaf1316d7ab2
Paul DeVito <pdevito3@gmail.com>
2022-05-07 19:27:30 -0400

    broken patchdoc toggle

--- RecipeManagement/src/RecipeManagement/Extensions/Services/WebAppServiceConfiguration.cs
+++ RecipeManagement/src/RecipeManagement/Extensions/Services/WebAppServiceConfiguration.cs
@@ -1,6 +1,9 @@
 namespace RecipeManagement.Extensions.Services;
 
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
 using Serilog;
 using Services;
 
@@ -14,14 +17,39 @@ public static class WebAppServiceConfiguration
         builder.Services.OpenTelemetryRegistration("RecipeManagement");
         builder.Services.AddInfrastructure(builder.Environment);
 
-        // using Newtonsoft.Json to support PATCH docs since System.Text.Json does not support them https://github.com/dotnet/aspnetcore/issues/24333
-        // if you are not using PatchDocs and would prefer to use System.Text.Json, you can remove The `AddNewtonSoftJson()` line
-        builder.Services.AddControllers()
-            .AddNewtonsoftJson()
-            .AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+        // using this will work
+        // builder.Services.AddControllers()
+        //     .AddNewtonsoftJson()
+        //     .AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+        
+        // using this will break
+        builder.Services.AddControllers(options =>
+        {
+            options.InputFormatters.Insert(0, CustomNewtonsoftJsonPatchInputFormatter.GetJsonPatchInputFormatter());
+        });
+        
         builder.Services.AddApiVersioningExtension();
         builder.Services.AddWebApiServices();
         builder.Services.AddHealthChecks();
         builder.Services.AddSwaggerExtension();
+    }
+}
+
+public static class CustomNewtonsoftJsonPatchInputFormatter
+{
+    public static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter()
+    {
+        var builder = new ServiceCollection()
+            .AddLogging()
+            .AddMvc()
+            .AddNewtonsoftJson()
+            .Services.BuildServiceProvider();
+
+        return builder
+            .GetRequiredService<IOptions<MvcOptions>>()
+            .Value
+            .InputFormatters
+            .OfType<NewtonsoftJsonPatchInputFormatter>()
+            .First();
     }
 }
