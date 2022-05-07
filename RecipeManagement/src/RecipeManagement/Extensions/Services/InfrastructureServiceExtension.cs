2fe33e8b902de864d8b95233cf1450cd850d5a59
Paul DeVito <pdevito3@gmail.com>
2022-05-07 19:24:34 -0400

    dev db for quicker local dx

--- RecipeManagement/src/RecipeManagement/Extensions/Services/InfrastructureServiceExtension.cs
+++ RecipeManagement/src/RecipeManagement/Extensions/Services/InfrastructureServiceExtension.cs
@@ -9,7 +9,7 @@ public static class ServiceRegistration
     public static void AddInfrastructure(this IServiceCollection services, IWebHostEnvironment env)
     {
         // DbContext -- Do Not Delete
-        if (env.IsEnvironment(LocalConfig.FunctionalTestingEnvName))
+        if (env.IsEnvironment(LocalConfig.FunctionalTestingEnvName) || env.IsDevelopment())
         {
             services.AddDbContext<RecipesDbContext>(options =>
                 options.UseInMemoryDatabase($"RecipeManagement"));
